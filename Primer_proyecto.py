7f198f17b5bb927bbe9c0d563e4308c290156b78
AlejandroVMonreal <alexx111.al@hotmail.es>
2025-09-01 12:46:47 -0600

    First proyect on python
    I made a little proyect where I only used input and print

---
+++ Primer_proyecto.py
@@ -0,0 +1,6 @@
+#print("El nombre de tu cerveza es: " + (input("cual es el nombre de tu mascota: ")) + " " + input("Cual es el nombre de tu ciudad: "))
+
+primer_nombre = input("Cual es el nombre de tu mascota: ")
+segundo_nombre = input("Cual es el nombre de tu amada: ")
+
+print("El nombre de tu cerveza es: " + primer_nombre  + " " + segundo_nombre)
