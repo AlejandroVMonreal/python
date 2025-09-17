63a4213adaf9db782cf2e4e0eadcbdb0db36d31e
AlejandroVMonreal <alexx111.al@hotmail.es>
2025-09-17 16:05:53 -0600

    ejercicios de python

---
+++ Day2/Formatear_cadenas.py
@@ -0,0 +1,5 @@
+x = 10
+y = 5
+
+print(f"mis numeros son {x} y {y}") #f nos sorve para poder concatenar nuestras variables
+print("Mis numeros son {} y {}").format(x, y)
---
+++ Day2/Rendondear.py
@@ -0,0 +1,5 @@
+resultado = round(90 / 7)
+print(resultado)
+
+valor = round(95.666666666666666666, 2)
+print(valor)
